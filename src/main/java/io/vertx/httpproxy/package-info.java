b00cc87408d4a4b5c4138a6c933eb966a098f306
Julien Viet <julien@julienviet.com>
2020-11-30 21:23:09 +0100

    Minor
    Signed-off-by: Julien Viet <julien@julienviet.com>

--- src/main/java/io/vertx/httpproxy/package-info.java
+++ src/main/java/io/vertx/httpproxy/package-info.java
@@ -12,7 +12,7 @@
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
-@ModuleGen(name = "vertx-reverse-proxy", groupPackage = "io.vertx")
+@ModuleGen(name = "vertx-http-proxy", groupPackage = "io.vertx")
 package io.vertx.httpproxy;
 
 import io.vertx.codegen.annotations.ModuleGen;
